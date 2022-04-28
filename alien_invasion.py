c6204a87c61488c3f6463b08e5f6e28f5d9125c9
pj <1626615930@qq.com>
2022-04-28 10:45:32 +0800

    二次修改

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,12 +1,15 @@
 import sys
 import pygame
 class AlienInvasion:
+
+
     """管理游戏资源和行为得类"""
     def __init__(self):
         """初始化游戏并创建游戏资源"""
         pygame.init()
-        self.screen=pygame.display.set_mode((1200,800))
+        self.screen=pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
+        self.bg_color=(230, 230, 230)
 
     def run_game(self):
         """开始游戏的主循环"""
@@ -15,6 +18,9 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type==pygame.QUIT:
                    sys.exit()
+
+#每次循环时都会重复屏幕
+            self.screen.fill(self.bg_color)
 # 让最近绘制的屏幕可见
             pygame.display.flip()
 
@@ -23,3 +29,4 @@ if _name_=='_main_':
 # 创建游戏实例并运行游戏
     ai=AlienInvasion()
     ai.run_game()
+
